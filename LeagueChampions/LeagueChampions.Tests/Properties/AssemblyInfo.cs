fadc008deee11a3ff5b50a05a945ed65107b7ec5
Thibault <phantomcat877@gmail.com>
2019-10-14 15:44:38 +0200

    map name change

--- LeagueChampions/LeagueChampions.Tests/Properties/AssemblyInfo.cs
+++ LeagueChampions/LeagueChampions.Tests/Properties/AssemblyInfo.cs
@@ -2,11 +2,11 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("NewProject.Tests")]
+[assembly: AssemblyTitle("LeagueChampions.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NewProject.Tests")]
+[assembly: AssemblyProduct("LeagueChampions.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
